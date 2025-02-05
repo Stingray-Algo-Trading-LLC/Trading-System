9490b1d186b7b630fa405d05739a20acdc28c504
Amjad Ali <a.z.ali@outlook.com>
2025-02-05 09:15:37 -0600

    Fixed errors/refactored.

--- Lib/LevelsAccel.hs
+++ Lib/LevelsAccel.hs
@@ -11,17 +11,16 @@ import Data.Array.Accelerate.LLVM.PTX as GPU -- or .LLVM.Native for CPU backend
 -- Functions to Compute Resistance Levels
 --------------------------------------------------------------------------------
 
-
 resistanceLevelsAcc :: Acc (Vector Double) -> Acc (Vector Double) -> Exp Double -> Exp Double -> Exp Double -> Exp Double -> Exp Double -> Acc (Vector Double)
 resistanceLevelsAcc barTopsVec barHighVec pillarThresh lowerBound upperBound rtol atol =
-  A.filter (A.> 0.0) qualifyingLevelsMaskVec
+  A.afst (A.filter (A.> 0.0) qualifyingLevelsMaskVec)
   where
-    areValsClose = isClose rtol atol 
+    areValsClose = isCloseAcc rtol atol 
     proximityMaskMat = generateProximityMaskMatrixAcc barHighVec barHighVec lowerBound upperBound areValsClose
     inBoundsMaskMat = generateInBoundsMaskMatrixAcc barTopsVec barHighVec areValsClose
     pillarMaskMat = andMatAcc proximityMaskMat inBoundsMaskMat
-    pillarSumVec = A.fold (+) 0.0 pillarMaskMat
-    qualifyingLevelsMaskVec = A.zipWith (\barHigh pillarSum -> cond (pillarSumn A.>= pillarThresh) barHighVec 0.0) barHighVec pillarSumVec
+    pillarSumVec = A.fold (A.+) 0.0 pillarMaskMat
+    qualifyingLevelsMaskVec = A.zipWith (\barHigh pillarSum -> A.cond (pillarSum A.>= pillarThresh) barHigh 0.0) barHighVec pillarSumVec
 
 
 generateProximityMaskMatrixAcc :: Acc (Vector Double) -> Acc (Vector Double) -> Exp Double -> Exp Double -> (Exp Double -> Exp Double -> Exp Double) -> Acc (Matrix Double)
@@ -43,35 +42,37 @@ generateInBoundsMaskMatrixAcc vecA vecB areValsClose =
     rightBoundaryPointsVec = generateRightBoundaryPointsVectorAcc boundaryMaskMat
   in
     generate (shape boundaryMaskMat) $ \ix ->
-      let Z :. row :. col = unlift ix :: Z :. Exp Int :. Exp Int 
-      in cond ((col A.> (leftBoundaryPointsVec ! (index1 row))) A.&& (col A.< (rightBoundaryPointsVec ! (index1 row)))) 1.0 0.0
+      let Z :. row :. col = A.unlift ix :: Z :. Exp Int :. Exp Int 
+      in A.cond ((A.fromIntegral col A.> (leftBoundaryPointsVec A.! A.index1 row)) A.&& (A.fromIntegral col A.< (rightBoundaryPointsVec A.! A.index1 row))) 1.0 0.0
 
 generateColIndxMaskMatAcc :: Acc (Matrix Double) -> Exp Double -> Acc (Matrix Double)
 generateColIndxMaskMatAcc maskMat defaultVal = 
   generate (shape maskMat) $ \ix ->
-    let Z :. row :. col = unlift ix :: Z :. Exp Int :. Exp Int
-    in cond ((maskMat ! ix) A.== 1.0) (A.fromIntegral col) (defaultVal)
+    let Z :. row :. col = A.unlift ix :: Z :. Exp Int :. Exp Int
+    in A.cond ((maskMat A.! ix) A.== 1.0) (A.fromIntegral col) defaultVal
 
 generateLeftBoundaryPointsVectorAcc :: Acc (Matrix Double) -> Acc (Vector Double) 
-generateLeftBoundaryPointsVectorAcc maskMat = fold1 A.max colIndxMaskMat
+generateLeftBoundaryPointsVectorAcc maskMat = A.fold1 A.max colIndxMaskMat
   where
-    colIndxMaskMat = generateColIndxMaskMatAcc maskMat (-1.0 :: Exp Double)
+    leftMaskMat = generateLowerTriMaskMatrixAcc maskMat
+    colIndxMaskMat = generateColIndxMaskMatAcc leftMaskMat (-1.0 :: Exp Double)
 
 
 generateRightBoundaryPointsVectorAcc :: Acc (Matrix Double) -> Acc (Vector Double)
-generateRightBoundaryPointsVectorAcc maskMat = fold1 A.min colIndxMaskMat
+generateRightBoundaryPointsVectorAcc maskMat = A.fold1 A.min colIndxMaskMat
   where
-    Z :. rows :. cols = unlift (shape maskMat) :: Z :. Exp Int :. Exp Int
-    colIndxMaskMat = generateColIndxMaskMatAcc maskMat $ A.fromIntegral cols
+    Z :. rows :. cols = A.unlift (shape maskMat) :: Z :. Exp Int :. Exp Int
+    rightMaskMat = generateUpperTriMaskMatrixAcc maskMat
+    colIndxMaskMat = generateColIndxMaskMatAcc rightMaskMat $ A.fromIntegral cols
 
 generateDiffMatrixAcc :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Matrix Double)
 generateDiffMatrixAcc vecA vecB = 
   let 
-    Z :. lenA = unlift (shape vecA) :: Z :. Exp Int
-    Z :. lenB = unlift (shape vecB) :: Z :. Exp Int 
-    matA = A.replicate (lift (Z :. All :. lenB)) vecA
-    matB = A.replicate (lift (Z :. lenA :. All)) vecB
-  in A.zipWith (-) matA matB
+    Z :. lenA = A.unlift (A.shape vecA) :: Z :. Exp Int
+    Z :. lenB = A.unlift (A.shape vecB) :: Z :. Exp Int 
+    matA = A.replicate (A.lift (Z :. All :. lenB)) vecA
+    matB = A.replicate (A.lift (Z :. lenA :. All)) vecB
+  in A.zipWith (A.-) matA matB
 
 
 -- Upper triangular mask
@@ -89,14 +90,8 @@ generateLowerTriMaskMatrixAcc matA =
         in A.cond (i A.> j) (matA A.! ix) 0.0
 
 
-generateLeftMaskMatrixAcc :: Acc (Matrix Double) -> Acc (Matrix Double)
-generateLeftMaskMatrixAcc maskMat = generateLowerTriMaskMatrixAcc maskMat
-
-generateRightMaskMatrixAcc :: Acc (Matrix Double) -> Acc (Matrix Double)
-generateRightMaskMatrixAcc maskMat = generateUpperTriMaskMatrixAcc maskMat
-
 isCloseAcc :: Exp Double -> Exp Double -> Exp Double -> Exp Double -> Exp Double
-isClose rtol atol valA valB = A.cond (v A.<= 1) 1.0 0.0
+isCloseAcc rtol atol valA valB = A.cond (v A.<= 1) 1.0 0.0
   where
     v = A.abs (valA A.- valB) A./ (atol A.+ rtol A.* valB)
 
@@ -123,4 +118,4 @@ isMatLTEAcc matA valB areValsClose =
 
 
 andMatAcc :: Acc (Matrix Double) -> Acc (Matrix Double) -> Acc (Matrix Double)
-andMatAcc maskMatA maskMatB = A.zipWith (*) maskMatA maskMatB
+andMatAcc = A.zipWith (A.*)
