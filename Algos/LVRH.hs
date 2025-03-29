0e3f32fe2f1dc4d6fd082eb7e9a96da39bef929c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2025-03-29 05:00:06 -0500

    Progress with trailing option strategy

--- Algos/LVRH.hs
+++ Algos/LVRH.hs
@@ -14,114 +14,6 @@ import Lib.LevelsAccel (initResistanceLevelsAcc)
 import Lib.Levels (resistanceLevels)
 import Data.List (sort)
 
-data LVRHState = LVRHState (forall y. StreamData -> (LVRHStateParam -> StreamData -> y) -> y)
-
-
-
-type Order = Bool
-data OrderOutput = O1 Order | O2 (Order, Int -> OrderOutput) | O3 (Int -> OrderOutput)
-
-instance Show OrderOutput where
-  show (O1 o) = "O1 " ++ show o
-  show (O2 (o, _)) = "O2 (" ++ show o ++ ", <function>)"
-  show (O3 _) = "O3 <function>"
-
-trailingTakeProfitStopLossState :: Double -> Double -> Double -> OrderOutput
-trailingTakeProfitStopLossState sl tk p -- stop-loss, take-profit, price
-  | p >= tk = O2 (True, trailingTakeProfitStopLossState (tk+10) (sl+10))
-  | p <= sl = O1 (False)
-  | otherwise = O3 (trailingTakeProfitStopLossState tk sl)
-
-marketBuyOrder
-func :: Int -> [OrderOutput] -> ([OrderOutput], [OrderOutput]) -> ([OrderOutput], [OrderOutput]) 
-func _ [] oldTup = oldTup
-func i (O3 f:fs) (o1Arr, o3Arr) = func i fs newTup
-  where
-    newTup = case f i of
-      O1 o1 -> (o1Arr ++ [O1 o1], o3Arr)
-      O2 (b, fx) -> (o1Arr ++ [O1 b], o3Arr ++ [O3 fx])
-      O3 o3 -> (o1Arr, o3Arr ++ [O3 o3])
-
-main :: IO ()
-main = do
-  let sellFuncs = map (\x -> O3 $ sellFunc x (x+5)) [1..20] 
-  
-  print $ func 8 sellFuncs ([],[])
-
-lvrhState :: LVRHStateParam -> StreamData -> (LVRHStateParam -> StreamData -> y) -> y
-lvrhState stateParams streamData func = func stateParams streamData
-
-lvrhStateTransition :: LVRHStateParam -> StreamData -> (StreamData -> (LVRHStateParam -> StreamData -> y) -> y)
-lvrhStateTransition stateParams (TradeData trade) =
-  if saleCondition == 0
-    then
-      lvrhState $
-        stateParams {firstTime = min (firstTime stateParams) currTradeTime}
-    else
-      lvrhState $
-        stateParams
-          { openPrice = if currTradeTime < openTime stateParams then price trade else openPrice stateParams,
-            highPrice = max (highPrice stateParams) (price trade),
-            lowPrice = min (lowPrice stateParams) (price trade),
-            closePrice = if (saleCondition == 1) && (currTradeTime >= lastTime stateParams) then price trade else closePrice stateParams,
-            openTime = min (openTime stateParams) currTradeTime,
-            firstTime = min (firstTime stateParams) currTradeTime,
-            lastTime = if saleCondition == 1 then max (lastTime stateParams) currTradeTime else lastTime stateParams
-          }
-  where
-    saleCondition = getSaleCondition $ condition trade
-    currTradeTime = utcToUnixSeconds $ tradeTimestamp trade
-lvrhStateTransition stateParams (BarData bar) =
-  lvrhState $
-    stateParams
-      { openPrice = 1/0, -- infinity
-        highPrice = -(1/0), -- -infinity
-        lowPrice = 1/0, -- infinity
-        closePrice = 0.0,
-        openTime = 1/0, -- infinity
-        firstTime = 1/0, -- infinity
-        lastTime = -(1/0), -- -infinity
-        barTops = newBarTops,
-        barHighs = newBarHighs,
-        resLevels = newResLevels -- getResLevels newBarTops newBarHighs
-      }
-  where
-    newBarTops = barTops stateParams ++ [max (open bar) (close bar)]
-    newBarHighs = barHighs stateParams ++ [high bar]
-    newResLevels = sort $ (genResLevels stateParams) newBarHighs newBarHighs
-
-lvrhBuyLogic :: Bool -> LVRHStateParam -> StreamData -> Bool
-lvrhBuyLogic buyStateParams stateParams (BarData bar) = False
-lvrhBuyLogic buyStateParams stateParams (TradeData trade) = 
-  not buyStateParams &&
-  hasDelayTimeElapsed stateParams 
-    (firstTime stateParams) (lastTime stateParams) && -- Wait time to ensure open price is accurate.
-  isHammer stateParams 
-    (openPrice stateParams) (highPrice stateParams) (lowPrice stateParams) (closePrice stateParams) &&
-  inDeflectZone stateParams 
-    (lowPrice stateParams) (closePrice stateParams) (resLevels stateParams)  -- Wick "touches" or is near resistance level.
-
-initLVRH :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> LVRHState
-initLVRH minResPillars resTolerance minBodyLen maxUpperWickLen minLowerWickLen deflecTolerance delay rtol atol =
-  lvrhState $ LVRHStateParam 
-    {
-      openPrice = 1/0, 
-      highPrice = -(1/0),
-      lowPrice = 1/0, 
-      closePrice = 0.0,
-      openTime = 1/0,
-      firstTime = 1/0, 
-      lastTime = -(1/0),
-      barTops = [], 
-      barHighs = [],
-      resLevels = [], 
-      genResLevels = initResistanceLevelsAcc minResPillars resTolerance rtol atol,
-      hasDelayTimeElapsed = \firstTime lastTime -> lastTime - firstTime >= delay :: Double -> Double -> Bool,
-      isHammer = isGreenHammer minBodyLen maxUpperWickLen minLowerWickLen,
-      inDeflectZone = isHammerInResDeflectZone deflecTolerance
-    }
-
-
 data OhlcState
   = OhlcState
   { openPrice :: Double, -- First qualifying trade price.
@@ -245,6 +137,90 @@ orderStateMachine constSt initOrderSt =
           isHammer constSt 
             (openPrice ohlcSt) (highPrice ohlcSt) (lowPrice ohlcSt) (closePrice ohlcSt) &&
           isDeflec
-        qty = fromIntegral (fromEnum shouldBuy) * (buyQty orderSt)
+        qty = (fromEnum shouldBuy) * (buyQty orderSt)
 
-    
+type Order = Bool
+data OrderOutput = O1 Order | O2 (Order, Int -> OrderOutput) | O3 (Int -> OrderOutput)
+
+trailingOptionOrder :: [OpenOrder] -> StreamData -> Int -> Double -> Double -> ([Order], OrderStrategy)
+trailingOptionOrder openOrders streamData buyQty initStopLoss initTakeProf
+  | noOpenOrders && noBuy = ([], nextStrategy [])
+  | noOpenOrders && haveBuy =
+    ([marketBuy], 
+      OrderStrategy $ 
+        trailingOptionOrder [newOpenOrder])
+  | haveOpenOrders && noBuy = 
+    let (orders, newOpenOrders) = queryOpenOrders ([],[]) openOrders price
+    in (orders, nextStrategy newOpenOrders)
+  | haveOpenOrders && haveBuy =
+    let initOrderTup = 
+      ([marketBuy], 
+        [newOpenOrder])
+        (orders, newOpenOrders) = queryOpenOrders initOrderTup openOrders price
+    in (orders, nextStrategy newOpenOrders)
+  where
+    haveBuy = buyQty > 0
+    noBuy = buyQty == 0
+    haveOpenOrders = not $ null openOrders
+    noOpenOrders = null openOrders
+    optionSymb = case streamData of
+      (TradeData t) -> 
+    marketBuy = genMarketBuy optionSymb buyQty
+    newOpenOrder = f (makeOptionSymbol streamData) buyQty initStopLoss initTakeProf
+    nextStrategy = OrderStrategy . trailingOptionOrder
+
+instance Show OrderOutput where
+  show (O1 o) = "O1 " ++ show o
+  show (O2 (o, _)) = "O2 (" ++ show o ++ ", <function>)"
+  show (O3 _) = "O3 <function>"
+
+trailingTakeProfitStopLossState :: Double -> Double -> Double -> OrderOutput
+trailingTakeProfitStopLossState sl tk p -- stop-loss, take-profit, price
+  | p >= tk = O2 (True, trailingTakeProfitStopLossState (tk+10) (sl+10))
+  | p <= sl = O1 (False)
+  | otherwise = O3 (trailingTakeProfitStopLossState tk sl)
+
+
+marketBuyOrder
+func :: Int -> [OrderOutput] -> ([OrderOutput], [OrderOutput]) -> ([OrderOutput], [OrderOutput]) 
+func _ [] oldTup = oldTup
+func i (O3 f:fs) (o1Arr, o3Arr) = func i fs newTup
+  where
+    newTup = case f i of
+      O1 o1 -> (o1Arr ++ [O1 o1], o3Arr)
+      O2 (b, fx) -> (o1Arr ++ [O1 b], o3Arr ++ [O3 fx])
+      O3 o3 -> (o1Arr, o3Arr ++ [O3 o3])
+
+main :: IO ()
+main = do
+  let sellFuncs = map (\x -> O3 $ sellFunc x (x+5)) [1..20] 
+  
+  print $ func 8 sellFuncs ([],[])type Order = Bool
+data OrderOutput = O1 Order | O2 (Order, Int -> OrderOutput) | O3 (Int -> OrderOutput)
+
+instance Show OrderOutput where
+  show (O1 o) = "O1 " ++ show o
+  show (O2 (o, _)) = "O2 (" ++ show o ++ ", <function>)"
+  show (O3 _) = "O3 <function>"
+
+trailingTakeProfitStopLossState :: Double -> Double -> Double -> OrderOutput
+trailingTakeProfitStopLossState sl tk p -- stop-loss, take-profit, price
+  | p >= tk = O2 (True, trailingTakeProfitStopLossState (tk+10) (sl+10))
+  | p <= sl = O1 (False)
+  | otherwise = O3 (trailingTakeProfitStopLossState tk sl)
+
+marketBuyOrder
+func :: Int -> [OrderOutput] -> ([OrderOutput], [OrderOutput]) -> ([OrderOutput], [OrderOutput]) 
+func _ [] oldTup = oldTup
+func i (O3 f:fs) (o1Arr, o3Arr) = func i fs newTup
+  where
+    newTup = case f i of
+      O1 o1 -> (o1Arr ++ [O1 o1], o3Arr)
+      O2 (b, fx) -> (o1Arr ++ [O1 b], o3Arr ++ [O3 fx])
+      O3 o3 -> (o1Arr, o3Arr ++ [O3 o3])
+
+main :: IO ()
+main = do
+  let sellFuncs = map (\x -> O3 $ sellFunc x (x+5)) [1..20] 
+  
+  print $ func 8 sellFuncs ([],[])
